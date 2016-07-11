913b68b844a2ba0502e4e6f83033de86be365a11
Holic101 <dietrich.kriworotow@gmail.com>
2016-07-11 15:37:08 +0200

    clickhandler and reset function added

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,17 +1,11 @@
-//IIFE for immediate start
-(function() {
+function init () {
 
   // Vars
   var playerToken = "X",
       aiToken = "O",
-      rows = 3,
-      cols = 3,
-      playing = false,
-      grid = new Array(rows),
-      aiTurn = false,
       board = new Array(9),
       playerTurn = false,
-      playerStart = false,
+      cellArray = document.getElementsByTagName("td"),
       winners = [
               [0, 1, 2],
               [3, 4, 5],
@@ -21,11 +15,56 @@
               [2, 5, 8],
               [0, 4, 8],
               [2, 4, 6],
-          ];
+          ],
+          //initialize output textfields
+          status = document.getElementById("status"),
+          winner = document.getElementById("winner");
 
-  //initialize output textfields
-  status = document.getElementById("status");
-  winner = document.getElementById("winner");
+//setup reset button
+document.getElementById("reset").addEventListener("click", clearGrid);
+
+  //token choice for the player -- IIFE
+    (function() {
+            var chooseToken = document.getElementById("chooseToken");
+            var choiceBtn = document.querySelector(".chooseTokenContent");
+            chooseToken.style.display = "block";
+            choiceBtn.addEventListener("click", function(e) {
+                playerToken = e.target.value;
+                if (playerToken == "X"){
+                  aiToken = "O";
+                }
+                else {
+                  aiToken = "X";
+                }
+                chooseToken.style.display = "none";
+                if (playerToken === "O") {
+                    playerTurn = false;
+                    play();
+                } else {
+                    whosTurnIsIt();
+                }
+            });
+        })();
+
+//set up cell click handler
+(function() {
+  for (var i = 0; i<cellArray.length; i++ ){
+    cellArray[i].addEventListener("click", cellClickHandler);
+  }
+  function cellClickHandler(e){
+    if (e.target.innerHTML === ""){
+    //fill out clicked cell with user token
+    e.target.innerHTML = playerToken;
+    //add clicked cell to board array
+    board[e.target.id] = playerToken;
+    }
+    //set AI up for next turn
+    playerTurn=false;
+    //call function for AI move
+    play();
+  }
+}) ();
+
 
   //Clear grid
   function clearGrid() {
@@ -38,20 +77,10 @@
 
   //run the game
   function play() {
-    if (aiTurn){
+    if (!playerTurn){
   //ToDo: Implement AI algorithm
     }
     aiTurn = false;
-  }
-
-  //Handle clicks in cells
-  function cellClickHandler(e) {
-    if (this.innerHTML === ""){
-    this.innerHTML = playerToken;
-    board[this.id] = playerToken;
-    }
-    aiTurn=true;
-    play();
   }
 
   //check for a winner
@@ -60,31 +89,11 @@
     document.getElementById("winner").innerHTML = "You win!";
     document.getElementById("winner").innerHTML = "You lose!";
   }
-//token choice for the player
-  (function() {
-          var chooseToken = document.getElementById("chooseToken");
-          var choiceBtn = document.querySelector(".chooseTokenContent");
-          chooseToken.style.display = "block";
-          choiceBtn.addEventListener("click", function(e) {
-              playerToken = e.target.value;
-              if (playerToken == "X"){
-                aiToken = "O";
-              }
-              else {
-                aiToken = "X";
-              }
-              chooseToken.style.display = "none";
-              if (playerToken === "O") {
-                  playerTurn = false;
-                  playerStart = false;
-                  play();
-              } else {
-                  whosTurnIsIt();
-              }
-          });
-      })();
 
-      function whosTurnIsIt() {
-              status.innerHTML = "Your turn: " + userToken;
+
+//display prompt for next player
+function whosTurnIsIt() {
+              status.innerHTML = "Your turn: " + playerToken;
           }
-})();
+}
+window.onload = init();
